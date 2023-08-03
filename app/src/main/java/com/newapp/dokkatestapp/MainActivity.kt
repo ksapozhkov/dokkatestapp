831f7e62f790cb8a7e7df71ccae17f2cfe147c98
Kirill Sapozhkov <kirill.sap@gmail.com>
2023-08-03 10:19:51 +0300

    Init

---
+++ app/src/main/java/com/newapp/dokkatestapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.newapp.dokkatestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+/**
+ * Тестовый комментарий Dokka
+ *
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
